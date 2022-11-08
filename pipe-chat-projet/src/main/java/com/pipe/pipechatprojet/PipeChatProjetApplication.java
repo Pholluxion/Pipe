385115afa762ad51ab853a50fa933910775d2939
CristianTafur249 <tafurcristian196@gmail.com>
2022-11-08 11:21:56 -0500

    primer uso de spring

---
+++ pipe-chat-projet/src/main/java/com/pipe/pipechatprojet/PipeChatProjetApplication.java
@@ -0,0 +1,13 @@
+package com.pipe.pipechatprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeChatProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeChatProjetApplication.class, args);
+	}
+
+}
