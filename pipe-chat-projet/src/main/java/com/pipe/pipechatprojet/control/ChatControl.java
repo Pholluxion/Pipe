0b5ed157befd2b13e03290464e099f3ed0d2b934
CristianTafur249 <tafurcristian196@gmail.com>
2022-11-13 16:04:12 -0500

    creacion de los modelos y control del micro servicio

--- pipe-chat-projet/src/main/java/com/pipe/pipechatprojet/control/ChatControl.java
+++ pipe-chat-projet/src/main/java/com/pipe/pipechatprojet/control/ChatControl.java
@@ -1,8 +1,29 @@
 package com.pipe.pipechatprojet.control;
 
 
+import com.pipe.pipechatprojet.control.modelo.Mensage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatControl {
+    
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    
+    @MessageMapping("/mensage")// /app/mensage
+    @SendTo("/chat/public")
+    public Mensage receivePublicMensage(@Payload Mensage mensage){
+        return mensage;
+    }
+    
+    @MessageMapping("/private-mensage")
+    private Mensage receivePrivateMensage(@Payload Mensage mensage){
+        simpMessagingTemplate.convertAndSendToUser(mensage.getReceiverName(),"/private",mensage);// /user/cristian/private
+        return mensage;
+    }
 }
---
+++ pipe-chat-projet/src/main/java/com/pipe/pipechatprojet/control/modelo/Status.java
@@ -0,0 +1,7 @@
+package com.pipe.pipechatprojet.control.modelo;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
