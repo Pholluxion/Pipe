85d039763864c36988ed59be786f50dd0bc1ade9
Pholluxion <penalozadaniel00@gmail.com>
2022-11-26 20:48:47 -0500

    Added Login and register service

--- microservices/auth_service/src/main/java/com/phollux/auth/security/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.phollux.auth.security;
-
-public class SecurityConstants {
-    public static final long JWT_EXP_DATE = 70000;
-    public static final String JWT_SECRET = "secret_key_for_jtw_token_generator";
-}
